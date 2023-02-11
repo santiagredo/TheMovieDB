1c447d88161813a1a97a68c3a99318cfb2c71fc5
Santiago S <santiagoagredo@live.com>
2023-02-11 18:25:07 -0500

    Removed secrets from gitignore

---
+++ src/secrets.mjs
@@ -0,0 +1,7 @@
+export const API_KEY = 'fa8b0a28fa9b9bdce4dcd1b2a3b1a430';
+
+
+//  Done:   0. Fix loading order in home
+//  Done:   1. Make page responsive
+//  2. Implement loading page/spinner/skeleton
+//  3. Implement crash/failed error message
